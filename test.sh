78cf19b7bf66100c34d213780e7f628bf758d276
Stephen Keller <stephen.keller@cisecurity.org>
2022-06-17 12:33:02 -0400

    wip25

--- test.sh
+++ test.sh
@@ -9,9 +9,9 @@ if [ ! -z "$platform" ]; then
     class=`sed -e 's/^"//' -e 's/"$//' <<<"$class"`
     if [ ! -z "$class" ]; then
         echo "python ./scripts/build_oval_definitions_file.py --outfile=\"./output-files/5.10/$class/$platform_filename.xml\" --max_schema_version=5.10 --platform=\"$platform\" --class=$class"
-#        python3 ./scripts/build_oval_definitions_file.py --outfile="./output-files/5.10/$class/$platform_filename.xml" --max_schema_version=5.10 --platform="$platform" --class=$class
-#        python3 ./scripts/build_oval_definitions_file.py --outfile="./output-files/5.11.1/$class/$platform_filename.xml" --max_schema_version=5.11.1 --platform="$platform" --class=$class
-#        python3 ./scripts/build_oval_definitions_file.py --outfile="./output-files/5.11.2/$class/$platform_filename.xml" --max_schema_version=5.11.2 --platform="$platform" --class=$class
+        python3 ./scripts/build_oval_definitions_file.py --outfile="./output-files/5.10/$class/$platform_filename.xml" --max_schema_version=5.10 --platform="$platform" --class=$class > /dev/null
+        python3 ./scripts/build_oval_definitions_file.py --outfile="./output-files/5.11.1/$class/$platform_filename.xml" --max_schema_version=5.11.1 --platform="$platform" --class=$class > /dev/null
+        python3 ./scripts/build_oval_definitions_file.py --outfile="./output-files/5.11.2/$class/$platform_filename.xml" --max_schema_version=5.11.2 --platform="$platform" --class=$class > /dev/null
     fi
   done
 fi
